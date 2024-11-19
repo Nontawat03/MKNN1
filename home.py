65fbfafbf428a8d63aa11b0814552cfa354b3289
Nontawat03 <155850776+Nontawat03@users.noreply.github.com>
2024-11-19 14:49:56 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,12 @@
 import streamlit as st
-import pandas as pandas
+import pandas as pd
 
 st.title("❤️❤️Website Developing using Python❤️❤️")
 st.header("🚗🚗Website Developing using Python🚗🚗")
 
+st.image('./img/myphoto.jpg')
 st.subheader("Nontawat Tingklongton")
+
+dt=pd.read_csv('./data/iris-3.csv')
+st.header()
+st.write(dt.head(10))
