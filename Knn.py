5e285768f60ef4170f757545cab099ae17bdf80a
Nontawat03 <155850776+Nontawat03@users.noreply.github.com>
2024-11-19 15:49:21 +0700

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -3,8 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
 
 st.header('Nontawat')
 st.image("./img/myphoto.jpg")
